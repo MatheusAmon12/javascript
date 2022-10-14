4ce521cd322f65b350807d3e31c13030043e0dce
MatheusAmon12 <111083094+MatheusAmon12@users.noreply.github.com>
2022-10-14 17:53:38 -0300

    Comecei a resolução de alguns desafios

---
+++ desafios/desafio1/codigo/ambiente.js
@@ -0,0 +1,42 @@
+let nome = 'MATHEUS FERREIRA'
+let nacionalidade = 'BRASILEIRO'
+let idade = 22
+let peso = 88.8
+let altura = 1.86
+
+document.write (`<div class="container">
+                    <h1 class="titulo">Lutador</h1>
+                    <div id="img">
+                        <img src="imagens/lutador.jpg" alt="lutador" id="imagem">
+                    </div>
+                    <div class="dados" id="nome">
+                        <p>${nome}</p>
+                    </div>
+                    <div class="dados" id="nacionalidade">
+                        <p>${nacionalidade}</p>
+                    </div>
+                    <div class="dados" id="idade">
+                        <p>${idade} anos</p>
+                    </div>
+                    <div class="dados" id="peso">
+                        <p>${peso} Kg</p>
+                    </div>
+                    <div class="dados" style="margin-bottom: 25px;" id="altura">
+                        <p>${altura} m</p>
+                    </div>
+                </div>`)
+
+let data = new Date()
+let dia = data.getDay()
+let mes = data.getMonth()
+let ano = data.getFullYear()
+
+document.write (`<div class="papel">
+                    <h1 id="titulo">Mensagem do dia</h1>
+                    <p id="data">${dia}/${mes}/${ano}</p>
+                    <p id="txt">"Seja curioso. Leia de tudo. Tente coisas novas. O que as pessoas chamam de inteligência se resume a curiosidade."</p>
+                    <p id="autor">Aaron Swartz</p>
+                </div>
+`)
+
+
